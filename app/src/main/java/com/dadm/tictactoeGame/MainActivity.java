9e1723fd8888325fd634fce7a5278dd3cdc3c79e
Brayan Portela <32397114+bjportelac@users.noreply.github.com>
2023-09-27 17:24:45 -0500

    Reto 3 - Upload
     Modified until Reto 3

--- app/src/main/java/com/dadm/tictactoeGame/MainActivity.java
+++ app/src/main/java/com/dadm/tictactoeGame/MainActivity.java
@@ -1,78 +1,120 @@
 package com.dadm.tictactoeGame;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.dadm.tictactoeGame.databinding.ActivityMainBinding;
-
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+
+    private TicTacToeGame mGame;
+    private Button[] mBoardButtons;
+    private TextView mInfoTextView;
+    private TextView mScoreTextView;
+    private int[] mScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
 
-        /*binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        mScore = new int[2];
+        mScore[0] = 1; mScore[1] = 1;
 
-        setSupportActionBar(binding.toolbar);
+        mBoardButtons = new Button[TicTacToeGame.BOARD_SIZE];
+        mBoardButtons[0] = (Button) findViewById(R.id.one);
+        mBoardButtons[1] = (Button) findViewById(R.id.two);
+        mBoardButtons[2] = (Button) findViewById(R.id.three);
+        mBoardButtons[3] = (Button) findViewById(R.id.four);
+        mBoardButtons[4] = (Button) findViewById(R.id.five);
+        mBoardButtons[5] = (Button) findViewById(R.id.six);
+        mBoardButtons[6] = (Button) findViewById(R.id.seven);
+        mBoardButtons[7] = (Button) findViewById(R.id.eight);
+        mBoardButtons[8] = (Button) findViewById(R.id.nine);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        mInfoTextView = (TextView) findViewById(R.id.information);
+        mScoreTextView = (TextView) findViewById(R.id.score);
+        mGame = new TicTacToeGame();
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+        startNewGame();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        // getMenuInflater().inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu);
+        menu.add(R.string.New_game);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        /*int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);*/
-        return false;
+        startNewGame();
+        return true;
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();*/
-        return false;
+    private void startNewGame(){
+        mGame.clearBoard();
+        for (int i = 0; i < mBoardButtons.length; i++) {
+            mBoardButtons[i].setText("");
+            mBoardButtons[i].setEnabled(true);
+            mBoardButtons[i].setOnClickListener(new ButtonClickListener(i));
+        }
+        mInfoTextView.setText(R.string.Human_turn);
+    }
+
+    private void setMove(char player, int location) {
+        mGame.setMove(player, location);
+        mBoardButtons[location].setEnabled(false);
+        mBoardButtons[location].setText(String.valueOf(player));
+        if (player == TicTacToeGame.HUMAN_PLAYER)
+            mBoardButtons[location].setTextColor(Color.rgb(0, 200, 0));
+        else
+            mBoardButtons[location].setTextColor(Color.rgb(200, 0, 0));
+    }
+
+
+    private class ButtonClickListener implements View.OnClickListener {
+        int location;
+
+        public ButtonClickListener(int location) {
+            this.location = location;
+        }
+
+        public void onClick(View view) {
+            if (mBoardButtons[location].isEnabled()) {
+                setMove(TicTacToeGame.HUMAN_PLAYER, location);
+
+                int winner = mGame.checkForWinner();
+                if (winner == 0) {
+
+                    mInfoTextView.setText(R.string.Computer_turn);
+                    int move = mGame.getComputerMove();
+                    setMove(TicTacToeGame.COMPUTER_PLAYER, move);
+                    winner = mGame.checkForWinner();
+                }
+
+                if (winner == 0){
+                    mInfoTextView.setText(R.string.Human_new_turn);
+                } else if (winner == 1){
+                    mInfoTextView.setText(R.string.Tie);
+                } else if (winner == 2){
+                    mInfoTextView.setText(R.string.Human_wins);
+                    mScore[0] = mScore[0] +1;
+                    mScoreTextView.setText("Player: "+mScore[0]+" "+" Android: "+mScore[1]);
+                } else {
+                    mInfoTextView.setText(R.string.Computer_wins);
+                    mScore[1] = mScore[1] +1;
+                    mScoreTextView.setText("Player: "+mScore[0]+" "+" Android: "+mScore[1]);
+                }
+            }
+        }
+
     }
 }
