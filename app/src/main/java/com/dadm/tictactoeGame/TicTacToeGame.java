858f3488fb9c79f956c14a1f55c2ca883b1c303b
Brayan Portela <32397114+bjportelac@users.noreply.github.com>
2023-09-27 16:03:20 -0500

    Test 1

---
+++ app/src/main/java/com/dadm/tictactoeGame/TicTacToeGame.java
@@ -0,0 +1,143 @@
+package com.dadm.tictactoeGame;
+
+import java.util.Random;
+
+public class TicTacToeGame {
+
+    public enum DifficultyLevel{Easy, Hard, Expert}
+
+    public static final int BOARD_SIZE = 9;
+    private final char[] mBoard = new char[BOARD_SIZE];
+
+    public static final char HUMAN_PLAYER = 'X';
+    public static final char COMPUTER_PLAYER = 'O';
+    public static final char OPEN_SPOT = ' ';
+
+    private final Random mRand;
+    public DifficultyLevel mDifficultyLevel = null;
+
+    public TicTacToeGame() {
+        mDifficultyLevel = DifficultyLevel.Hard;
+        mRand = new Random();
+    }
+
+    /** Clear the board of all X's and O's by setting all spots to OPEN_SPOT. */
+    public void clearBoard(){
+        for(int i = 0; i < BOARD_SIZE; i++){
+            mBoard[i] = OPEN_SPOT;
+        }
+    }
+
+    /** Set the given player at the given location on the game board.
+     * The location must be available, or the board will not be changed.
+     *
+     * @param player - The HUMAN_PLAYER or COMPUTER_PLAYER
+     * @param location - The location (0-8) to place the move
+     */
+    public void setMove(char player, int location){
+        if(location < BOARD_SIZE && mBoard[location] == OPEN_SPOT){
+            mBoard[location] = player;
+        }
+    }
+
+    /**
+     * Check for a winner and return a status value indicating who has won.
+     * @return Return 0 if no winner or tie yet, 1 if it's a tie, 2 if X won,
+     * or 3 if O won.
+     */
+    public int checkForWinner(){
+        int[][] winCombinations = new int[][] {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+                {0, 4, 8}, {2, 4, 6}
+        };
+        for (int[] combination : winCombinations) {
+            if (mBoard[combination[0]] == HUMAN_PLAYER && mBoard[combination[1]] == HUMAN_PLAYER && mBoard[combination[2]] == HUMAN_PLAYER)
+                return 2;
+            if (mBoard[combination[0]] == COMPUTER_PLAYER && mBoard[combination[1]] == COMPUTER_PLAYER && mBoard[combination[2]] == COMPUTER_PLAYER)
+                return 3;
+        }
+
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if (mBoard[i] != HUMAN_PLAYER && mBoard[i] != COMPUTER_PLAYER)
+                return 0;
+        }
+        return 1;
+    }
+
+    /**
+     * Get a random move for the computer.
+     * @return A random move for the computer (0-8).
+     */
+    public int getRandomMove(){
+        int move;
+        do{
+            move = mRand.nextInt(BOARD_SIZE);
+        }while(mBoard[move] != OPEN_SPOT);
+        return move;
+    }
+
+    /**
+     * Get a winning move for the computer if available.
+     * @return The winning move if available, otherwise -1.
+     */
+    public int getWinningMove(){
+        for(int i = 0; i < BOARD_SIZE; i++){
+            if(mBoard[i] == OPEN_SPOT){
+                char current = mBoard[i];
+                mBoard[i] = COMPUTER_PLAYER;
+                if(checkForWinner() == 3){
+                    mBoard[i] = OPEN_SPOT;
+                    return i;
+                }
+                mBoard[i] = current;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Get a blocking move to prevent the human player from winning.
+     * @return The blocking move if available, otherwise -1.
+     */
+    public int getBlockingMove() {
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            if (mBoard[i] == OPEN_SPOT) {
+                char current = mBoard[i];
+                mBoard[i] = HUMAN_PLAYER;
+                if (checkForWinner() == 2) {
+                    mBoard[i] = OPEN_SPOT; // Restore the board state
+                    return i;
+                }
+                mBoard[i] = current;
+            }
+        }
+        return -1; // No blocking move found
+    }
+
+
+    public int getComputerMove(){
+        int move = -1;
+        if(mDifficultyLevel == DifficultyLevel.Easy){
+            move = getRandomMove();
+        } else if(mDifficultyLevel == DifficultyLevel.Hard){
+            move = getWinningMove();
+            if(move == -1){
+                move = getRandomMove();
+            }
+        } else if (mDifficultyLevel == DifficultyLevel.Expert) {
+            // Try to win, but if that's not possible, block.
+            // If that's not possible, move anywhere.
+            move = getWinningMove();
+            if(move == -1){
+                move = getBlockingMove();
+                if(move == -1){
+                    move = getRandomMove();
+                }
+            }
+        }
+        return move;
+    }
+
+
+}
