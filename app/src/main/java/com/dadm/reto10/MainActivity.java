e367eb832d8c6bf64d552c315f663a9ee66870f0
Brayan Portela <32397114+bjportelac@users.noreply.github.com>
2023-11-30 00:46:31 -0500

    Done
    Tthanks to a grat friend of mine!

---
+++ app/src/main/java/com/dadm/reto10/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dadm.reto10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dadm.reto10.;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
